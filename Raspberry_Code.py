24eeff8d6efddfaa84c68b3b2dcf618816c2af2c
1701aman <62381699+1701aman@users.noreply.github.com>
2025-01-02 02:23:04 +0530

    Update Raspberry_Code.py

--- Raspberry_Code.py
+++ Raspberry_Code.py
@@ -1,6 +1,6 @@
 from smbus import SMBus
 
-addr = 0x8 # Bus address
+addr = 0x8 # Bus Address
 bus = SMBus(1) # indicates /dev/ic2-1
 bus.write_byte(addr, 0x1) # Switch it ON
 input("Press return to exit")
